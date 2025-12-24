c49e34ae358f72871435378c7c35135a32aea473
Amith Mohanan <legit@amo.fyi>
2025-12-24 11:02:33 +0000

    fix(hn-post-favicon): Use GM_addElement API to insert icons
    To bypass Content Security Policy

--- hn-post-favicon.user.js
+++ hn-post-favicon.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Hacker News post favicon
 // @namespace    https://amo.fyi
-// @version      1.0
+// @version      1.1
 // @description  Adds favicons from DDG to posts on Hacker News
 // @author       Amith M
 // @match        https://*.ycombinator.com/*
+// @grant        GM_addElement
 // @updateURL    https://raw.githubusercontent.com/amithm7/userscripts/refs/heads/main/hn-post-favicon.user.js
 // @downloadURL  https://raw.githubusercontent.com/amithm7/userscripts/refs/heads/main/hn-post-favicon.user.js
 // ==/UserScript==
@@ -14,11 +15,9 @@ const links = document.querySelectorAll('.titleline > a');
 for(let link of links) {
 	const domain = new URL(link.href).hostname;
 	const imageUrl = `https://icons.duckduckgo.com/ip3/${domain}.ico`;
-	const image = document.createElement('img');
-	image.src = imageUrl;
-	image.style.width = '1em';
-	image.style.height = '1em';
-	image.style.paddingRight = '.25em';
-	image.style.paddingLeft = '.25em';
-	link.prepend(image);
+    const image = GM_addElement(link, 'img', {
+        src: imageUrl,
+        style: 'width:1em; height:1em; padding-right:.25em; padding-left:.25em;'
+    });
+    link.prepend(image);
 }
