1adc14a218a9fc022db96e37a78c121b4e26081c
Amith Mohanan <legit@amo.fyi>
2026-01-31 10:10:52 +0530

    fix(vk-video-desktop-mobile-switch): update CSS selectors

--- vk-video-desktop-mobile-switch.user.js
+++ vk-video-desktop-mobile-switch.user.js
@@ -15,8 +15,8 @@
 (function () {
 	"use strict";
 
-	const desktopShareMenuSelector = ".vkitPostFooter__root--J16mh"; // ".videoplayer_share_actions";
-	const mobileShareMenuSelector = ".VideoPageCardActions__root--QTleF"; // "--nKtyl" ".VideoPageCardActions-module__root--ClPQo"
+	const desktopShareMenuSelector = ".vkitPostFooter__root--Pvcve"; // J16mh // ".videoplayer_share_actions";
+	const mobileShareMenuSelector = ".VideoPageCardActions__root--EOjur"; // QTleF // "nKtyl" // ".VideoPageCardActions-module__root--ClPQo"
 
 	// TODO: Display notifications on these below actiivites.
 	// TODO: Automatic redirect to mobile if video doesn't play in 10s ?
